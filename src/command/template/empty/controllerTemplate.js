0a592368e937fd213a444c06ad5c26406382a13d
vdanviel <victordn.araujo@gmail.com>
2025-09-24 20:08:34 -0300

    init

---
+++ src/command/template/empty/controllerTemplate.js
@@ -0,0 +1,10 @@
+import { ObjectId } from 'mongodb';
+import { __TitleModuleName__ } from "../model/__ModuleName__.js";
+import Util from "../util/util.js"; // Importa Util, utilitario do sistema
+
+class Controller {
+	
+}
+
+// Substitua '__TitleModuleName__' pelo model desejado ao gerar controller real
+export { Controller };
---
+++ src/router/version/version1Router.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+//rotas
+import { customerRouter } from "../customerRouter.js";
+import { personalAccessTokenRouter } from "../personalAcessTokenRouter.js";
+
+const version1Router = express.Router();
+
+version1Router.use(`/customer`, customerRouter);
+version1Router.use(`/token`, personalAccessTokenRouter);
+
+export {version1Router};
