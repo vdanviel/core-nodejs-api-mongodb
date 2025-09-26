87222605e1a37f9648f056c05e296c56e5f494e7
vdanviel <victordn.araujo@gmail.com>
2025-09-26 20:06:36 -0300

    refactor: command/template/complete/routerTemplate.js

--- src/command/template/empty/routerTemplate.js
+++ src/command/template/empty/routerTemplate.js
@@ -1,6 +1,8 @@
 
 import express from "express";
 import { __ModuleName__Controller } from "../controller/__ModuleName__Controller.js";
+import { body } from "express-validator";
+
 
 const __ModuleName__Router = express.Router();
 
